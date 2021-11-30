f789a5c0606af954fdc2f2479e6723624f1818db
Anton Tropin <antropy.n@gmail.com>
2021-11-30 14:55:25 +0700

    Base template and card creation prototype

--- ShumiProject/ShumiApp/views.py
+++ ShumiProject/ShumiApp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from .models import *
 
-# Create your views here.
+def card_create(request):
+    user = User.objects.get(pk = request.user.pk)
+
+    if request.method == "POST":
+        card = Card()
+        card.owner = user.profile
+        card.title = request.POST.get("card_title")
+        card.description = request.POST.get("card_description")
+        card.save()
+        # return redirect('')
+    else:
+        return render(request, "ShumiApp/card_create_page.html", {'title': 'Создать карточку'})
