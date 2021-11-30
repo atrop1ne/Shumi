c90f04ef85b4dbb9aad5d9a130f11835bd455284
Anton Tropin <antropy.n@gmail.com>
2021-11-30 16:21:21 +0700

    Main page prototype

--- ShumiProject/ShumiApp/views.py
+++ ShumiProject/ShumiApp/views.py
@@ -2,6 +2,17 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from .models import *
 
+def main(request):
+    user = User.objects.get(pk = request.user.pk)
+    cards = Card.objects.filter(owner = user.profile)
+
+    context = {
+        'title' : 'Главная',
+        'cards' : cards
+    }
+
+    return render(request, "ShumiApp/main.html", context)
+
 def card_create(request):
     user = User.objects.get(pk = request.user.pk)
 
@@ -11,6 +22,6 @@ def card_create(request):
         card.title = request.POST.get("card_title")
         card.description = request.POST.get("card_description")
         card.save()
-        # return redirect('')
+        # return redirect('main')
     else:
         return render(request, "ShumiApp/card_create_page.html", {'title': 'Создать карточку'})
