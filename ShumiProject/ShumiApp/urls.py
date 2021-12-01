5417776774bb876193717b2b14f07441b2acbf68
Anton Tropin <antropy.n@gmail.com>
2021-12-01 15:14:05 +0700

    Setup media root, saving and displaying images, update gitignore

--- ShumiProject/ShumiApp/urls.py
+++ ShumiProject/ShumiApp/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('main', views.main, name = 'main'),
     path('card_create', views.card_create, name = 'card_create'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
