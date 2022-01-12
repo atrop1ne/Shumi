5634e4f70594bbd0262c43ffe4f32fef70fb88a4
Anton Tropin <antropy.n@gmail.com>
2022-01-12 15:06:24 +0700

    Logo and models small mistake

--- ShumiProject/ShumiApp/models.py
+++ ShumiProject/ShumiApp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.urls import reverse
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.core.exceptions import ObjectDoesNotExist
